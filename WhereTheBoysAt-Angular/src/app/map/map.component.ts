ea58e389b7fdfb2dcc2d4870d800f2680d6a2181
TerranceDwan <terrance4869@gmail.com>
2020-10-28 10:06:35 -0400

    fixed bug on uState.features

--- WhereTheBoysAt-Angular/src/app/map/map.component.ts
+++ WhereTheBoysAt-Angular/src/app/map/map.component.ts
@@ -21,19 +21,27 @@ export class MapComponent implements OnInit {
   
   constructor() { }
 
+  getStateFeatures(uState){
+    return uState.features
+  }
+  getStateName(uState, i){
+    return uState.features[i].properties.name
+  }
+ 
+
   ngOnInit(): void {
     d3.json(this.url)
       .then((uState) => {
-        console.log(uState.features[0].properties.name)
+        console.log(this.getStateFeatures(uState))
         this.svg.selectAll('path')
-        .data(uState.features)
+        .data(this.getStateFeatures(uState))
         .enter()
         .append('path')
         .attr('d', this.path)
         .attr('class', 'state')
         let map = document.getElementById('map')
         for(let i = 0; i < map.childElementCount; i++){
-          map.children[i].setAttribute('id',uState.features[i].properties.name)
+          map.children[i].setAttribute('id',this.getStateName(uState, i))
         }
       })
       .catch((error) => {
