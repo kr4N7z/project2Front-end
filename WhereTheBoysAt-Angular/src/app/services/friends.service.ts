7bc005bb9af374ae0c0c65acff9e9b1616856c9c
TerranceDwan <terrance4869@gmail.com>
2020-11-04 09:32:51 -0500

    changed server point

--- WhereTheBoysAt-Angular/src/app/services/friends.service.ts
+++ WhereTheBoysAt-Angular/src/app/services/friends.service.ts
@@ -12,7 +12,7 @@ export class FriendsService {
   constructor(private httpClient:HttpClient) { }
 
   getAllFriends():Observable<Friend[]>{
-    //return this.httpClient.get("http://localhost:8088/WhereTheBoysAt/user/myfriends") as Observable<Friend[]>
+    // return this.httpClient.get("http://localhost:8080/WhereTheBoysAt/user/myfriends") as Observable<Friend[]>
     return this.httpClient.get("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/myfriends") as Observable<Friend[]>;
  }
 
--- WhereTheBoysAt-Angular/src/app/services/login.service.ts
+++ WhereTheBoysAt-Angular/src/app/services/login.service.ts
@@ -15,6 +15,6 @@ export class LoginService {
     //expecting login credentials to have email and password properties set.
     let body = JSON.stringify(loginCredentials);
     return this.httpClient.post("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/login", body,{headers:headers})  as Observable<Object>;
-    //return this.httpClient.post("http://localhost:8088/WhereTheBoysAt/user/login", body,{headers:headers})  as Observable<Object>;
+    // return this.httpClient.post("http://localhost:8080/WhereTheBoysAt/user/login", body,{headers:headers})  as Observable<Object>;
  }
 }
