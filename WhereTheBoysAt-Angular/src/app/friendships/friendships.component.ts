aa6a21775334d887eb3127b010ea766208c9eee4
Jonah Neugeboren <jonahpie@gmail.com>
2020-11-04 14:25:06 -0500

    adding mappings and add friend form

--- WhereTheBoysAt-Angular/src/app/friendships/friendships.component.ts
+++ WhereTheBoysAt-Angular/src/app/friendships/friendships.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FriendsService } from 'src/app/services/friends.service';
+import { Friend } from 'src/app/models/friend';
+import { FriendshipsService } from '../services/friendships.service';
 
 @Component({
   selector: 'app-friendships',
@@ -7,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendshipsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private friendsService:FriendsService, private friendshipsService:FriendshipsService) { }
+
+  emailToAdd;
+
+  addFriend() {
+
+  }
 
   ngOnInit(): void {
   }
