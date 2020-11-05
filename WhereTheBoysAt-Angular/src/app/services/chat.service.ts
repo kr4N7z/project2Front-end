905fe8641bdac15f37f51807a45b6185adcc5510
kr4N7z <73358932+kr4N7z@users.noreply.github.com>
2020-11-05 17:00:49 -0600

    service

--- WhereTheBoysAt-Angular/src/app/services/chat.service.ts
+++ WhereTheBoysAt-Angular/src/app/services/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Message } from '../models/message'
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,8 @@ export class ChatService {
       let headers:HttpHeaders = new HttpHeaders().set('Content-Type','application/json');
       //expecting login credentials to have email and password properties set.
       let body = JSON.stringify(loginCredentials);
-      return this.httpClient.post("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/login", body,{headers:headers, withCredentials:true})  as Observable<Friend>;
-      //return this.httpClient.post<Friend>("http://localhost:8088/WhereTheBoysAt/user/login", body,{headers:headers, withCredentials:true})  as Observable<Friend>;
+      return this.httpClient.post("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/messages/getMyMessages", body,{headers:headers, withCredentials:true})  as Observable<Message>;
+      //return this.httpClient.post<Friend>("http://localhost:8088/WhereTheBoysAt/messages/getMyMessages", body,{headers:headers, withCredentials:true})  as Observable<Message>;
    }
    
 }
