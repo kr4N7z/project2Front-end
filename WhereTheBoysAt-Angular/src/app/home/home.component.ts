ff9bb8f4c840e742dc31dc144fbcf8b2501b7224
Scotty Mikul <Scott.p.mikul@gmail.com>
2020-11-05 17:52:23 -0500

    registration/session testing / logout

--- WhereTheBoysAt-Angular/src/app/home/home.component.ts
+++ WhereTheBoysAt-Angular/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { LogoutService} from 'src/app/services/logout.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private logoutService:LogoutService, private route:Router) { }
 
   ngOnInit(): void {
   }
-
+  parseData(data){
+    console.log(data);
+    if(data.success){
+      console.log("success..... logging out now");
+      this.route.navigate(["/"]);
+    }else{
+      console.log("failed to logout");
+    }
+  }
+  logout(){
+    //console.log("logout is firing");
+    this.logoutService.logout().subscribe(
+      (data)=>{
+       this.parseData(data)
+      }
+    );
+  }
 }
---
+++ WhereTheBoysAt-Angular/src/app/services/logout.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogoutService {
+  constructor(private httpClient:HttpClient) { }
+  logout():Observable<Object>{
+    console.log("inside the logout service");
+    let headers:HttpHeaders = new HttpHeaders().set('Content-Type','application/json');
+    //expecting login credentials to have email and password properties set.
+   
+    //return this.httpClient.get("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/logout", body,{headers:headers})  as Observable<Object>;
+    return this.httpClient.get("http://localhost:8080/WhereTheBoysAt/user/logout", {headers:headers})  as Observable<Object>;
+  }
+
+}
