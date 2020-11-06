cf6f1eed82b1c1e66711b211583e1ec64fa3cc7e
Scotty Mikul <Scott.p.mikul@gmail.com>
2020-11-05 19:33:23 -0500

    fixed get

--- WhereTheBoysAt-Angular/src/app/services/logout.service.ts
+++ WhereTheBoysAt-Angular/src/app/services/logout.service.ts
@@ -12,8 +12,8 @@ export class LogoutService {
     let headers:HttpHeaders = new HttpHeaders().set('Content-Type','application/json');
     //expecting login credentials to have email and password properties set.
    
-    return this.httpClient.get("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/logout", body,{headers:headers})  as Observable<Object>;
-    //return this.httpClient.get("http://localhost:8080/WhereTheBoysAt/user/logout", {headers:headers, withCredentials:true})  as Observable<Object>;
+    //return this.httpClient.get("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/logout", {headers:headers})  as Observable<Object>;
+    return this.httpClient.get("http://localhost:8080/WhereTheBoysAt/user/logout", {headers:headers, withCredentials:true})  as Observable<Object>;
   }
 
 }
