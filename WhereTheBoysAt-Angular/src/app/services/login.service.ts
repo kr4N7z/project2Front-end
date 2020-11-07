9a62bfad0ea5281fecbd407008eaacbe25ee451f
TerranceDwan <terrance4869@gmail.com>
2020-11-07 17:58:40 -0500

    added routing

--- WhereTheBoysAt-Angular/src/app/services/login.service.ts
+++ WhereTheBoysAt-Angular/src/app/services/login.service.ts
@@ -15,7 +15,7 @@ export class LoginService {
     //expecting login credentials to have email and password properties set.
     let body = JSON.stringify(loginCredentials);
    //return this.httpClient.post<Friend>("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/login", body,{headers:headers, withCredentials:true})  as Observable<Friend>;
-   return this.httpClient.post<Friend>("http://localhost:8088/WhereTheBoysAt/user/login", body,{headers:headers, withCredentials:true})  as Observable<Friend>;
+   return this.httpClient.post<Friend>("http://localhost:8080/WhereTheBoysAt/user/login", body,{headers:headers, withCredentials:true})  as Observable<Friend>;
  }
 
 //  test(loginCredentials):Observable<Object>{
--- WhereTheBoysAt-Angular/src/app/services/update.service.ts
+++ WhereTheBoysAt-Angular/src/app/services/update.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders, HttpParams } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient){ }
+
+  updateUser(userId,email,firstName,lastName):Observable<any>{
+    let headers:HttpHeaders = new HttpHeaders().set('Content-Type','application/json');
+    let params = new HttpParams();
+    params = params.append('userId', userId);
+    params = params.append('email', email);
+    params = params.append('firstName', firstName);
+    params = params.append('lastName', lastName);
+
+    // return this.httpClient.post("http://localhost:8080/WhereTheBoysAt/user/update?userId=1&email=email@email.com&firstName=John&lastName="+lastName,{headers:headers, withCredentials:true})
+    return this.httpClient.post("http://localhost:8080/WhereTheBoysAt/user/update",{},{headers:headers, withCredentials:true,params:params})
+    // return this.httpClient.post("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/update",{headers:headers,withCredentials:true,params:params});
+ }
 }
