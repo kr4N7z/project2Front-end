fa988ecbfdab47311c381538e1f1a3a81cf75dd4
Scotty Mikul <Scott.p.mikul@gmail.com>
2020-11-08 19:06:26 -0500

    ec2 services

--- WhereTheBoysAt-Angular/src/app/services/register.service.ts
+++ WhereTheBoysAt-Angular/src/app/services/register.service.ts
@@ -13,7 +13,7 @@ export class RegisterService {
     //expecting login credentials to have email and password properties set.
     let body = JSON.stringify(registrationUser);
     console.log("sending: "+ body);
-    //return this.httpClient.post("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/register", body,{headers:headers})  as Observable<Object>;
-    return this.httpClient.post("http://localhost:8088/WhereTheBoysAt/user/register", body,{headers:headers})  as Observable<Object>;
+    return this.httpClient.post("http://ec2-54-237-35-242.compute-1.amazonaws.com:8088/WhereTheBoysAt/user/register", body,{headers:headers})  as Observable<Object>;
+    //return this.httpClient.post("http://localhost:8088/WhereTheBoysAt/user/register", body,{headers:headers})  as Observable<Object>;
  }
 }
